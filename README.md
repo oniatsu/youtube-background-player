0e0b69f1b7b57ad22470c44d26beacaa571ce5c9
oniatsu <5919569+oniatsu@users.noreply.github.com>
2017-03-05 10:50:54 +0900

    Initialize

---
+++ README.md
@@ -0,0 +1,3 @@
+# YouTube Background Player
+
+YouTube Background Player using Electron.
---
+++ preload.js
@@ -0,0 +1,37 @@
+const { ipcRenderer } = require('electron');
+
+onload = () => {
+    const target = document.querySelector('#player');
+    if (target === null) return;
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach((addedNode) => {
+                if (addedNode.nodeName === "VIDEO") {
+                    addedNode.loop = true;
+                }
+            });
+        })
+    }).observe(target, {
+        attributes: true,
+        childList: true,
+        subtree: true,
+        characterData: true,
+    });
+};
+
+ipcRenderer.on('menu-message', (event, args) => {
+    const video = document.querySelector("video");
+
+    if (args.togglePlay) {
+        if (video == null) return;
+
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+        }
+    } else if (args.openOnBrowser) {
+        // TODO: ブラウザで開く
+        document.url;
+    }
+});
